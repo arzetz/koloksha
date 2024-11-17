c151bfe71b357e1e0e288639b87703de3994096b
freeny-afk <artemkach@inbox.ru>
2024-11-17 23:53:58 +0300

    deploying v0.1

--- backend/models/models.go
+++ backend/models/models.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Order struct {
 	gorm.Model
-	User_Name    string `json:"user name" gorm:"text;not null;default:null"`
-	Phone_Number string `json:"phone number" gorm:"text;not null;default:null"`
+	User_Name    string `json:"name" gorm:"text;not null;default:null"`
+	Phone_Number string `json:"phone" gorm:"text;not null;default:null"`
 }
